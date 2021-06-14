86991b3487df62054f44cb448f3386d85dd6b7c7
Enricco Amaral <enricco.amaral@hotmail.com.br>
2021-06-14 15:25:53 -0300

    Commit inicial/ Primeira aula/ Servlet e html OiMundo | 14/06 03:25

---
+++ src/main/java/gerenciador/OiMundoServlet.java
@@ -0,0 +1,24 @@
+package gerenciador;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns="/oi")
+public class OiMundoServlet extends HttpServlet{
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("Hello World, sou o mais novo zupper :)");
+		out.println("<body>");
+		out.println("</html>");
+	}
+}
